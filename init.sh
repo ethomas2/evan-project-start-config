08eab400a34861c359031892a01c5220f556c167
Evan Thomas <evanrthom@gmail.com>
2025-12-18 13:34:18 -0800

    Update

---
+++ init.sh
@@ -0,0 +1,33 @@
+set -euxo pipefail
+SRC=/Users/evanthomas/github.com/ethomas2/evan-project-start-config
+DST=${1:-$(pwd)}
+cp -ri "$SRC"/* "$DST"/
+
+(
+  cd "$DST"
+  rm init.sh || true
+  mv -i cursor .cursor
+  mv -i gitignore .gitignore
+  mv -i envrc .envrc
+  direnv allow
+)
+
+(
+  cd $DST/backend/
+  touch requirements.txt
+  ~/.pyenv/versions/3.13.0/bin/python -m venv venv
+  source venv/bin/activate
+  pip install -r dev-requirements.txt
+)
+
+(
+  cd "$DST"
+
+  ln -s backend/venv
+  ln -s backend/pylintrc
+  ln -s backend/pyproject.toml
+
+  git init
+  git add .
+  git commit -m "Initial commit"
+)
